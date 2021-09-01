e31a9ffc6a009e68d944e1102cd8500ba3d78c2c
Verity <joshua-schmidt@online.de>
2021-09-01 14:12:02 +0200

    :wrench: Setup react (with typescript)

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p>Working</p>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
